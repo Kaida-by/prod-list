c21d06fbdcb52c848cac928fee2aaef23b6a3526
Denis Stolyarov <skwidw92@gmail.com>
2023-08-23 10:59:41 +0200

    hotfix/add_relationship | fixed some types and added new relationship

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property mixed $text
- * @property mixed $user_id
+ * @property string $text
+ * @property int $user_id
  * @method static findOrFail(int $int)
  */
 class Comment extends Model
--- backend/app/Models/TypeProduct.php
+++ backend/app/Models/TypeProduct.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property mixed $name
- * @property mixed $user_id
- * @property mixed $product_list_id
+ * @property string $name
+ * @property int $user_id
+ * @property int $product_list_id
  * @method static findOrFail(int $int)
  */
 class TypeProduct extends Model
